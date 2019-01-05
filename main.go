baf904ee7f03fb0137ead9805c7dc0f7fc47442e
urothis <natecaple@gmail.com>
2019-01-05 00:42:32 -0600

    .

--- main.go
+++ main.go
@@ -16,6 +16,65 @@ import (
 	"github.com/jasonlvhit/gocron"
 )
 
+var (
+	//RedisClient is a var
+	RedisClient *redis.Client
+)
+
+func startPubsub() {
+	cfg := config{}
+	err := env.Parse(&cfg)
+
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+	}
+
+	client := redis.NewClient(&redis.Options{
+		Addr: "redis:" + cfg.RedisPort,
+	})
+	defer client.Close()
+
+	pubSub := client.Subscribe(
+		"heartbeat",
+		"input",
+		"debug",
+		"github",
+	)
+	for {
+		msg, _ := pubSub.ReceiveMessage()
+		switch msg.Channel {
+		case "heartbeat":
+
+		case "input":
+
+		case "debug":
+		}
+	}
+}
+
+func sendPubsub(LogMessage string, PubsubChannel string, PubsubMessage string) {
+	cfg := config{}
+	err := env.Parse(&cfg)
+
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+	}
+
+	client := redis.NewClient(&redis.Options{
+		Addr: "redis:" + cfg.RedisPort,
+	})
+	defer client.Close()
+
+	if err := client.Publish(PubsubChannel, PubsubMessage).Err(); err != nil {
+		panic(err)
+	}
+
+	if cfg.HbVerbose == true {
+		fmt.Println(LogMessage)
+	}
+}
+
+
 func heartbeatWebhook(ticker string, verbose bool) {
 	t := time.Now()
 	msg := ("O [" + t.Format("15:04:05") + "] [NWN_Order] Pubsub Event: channel=heartbeat message=" + ticker)
