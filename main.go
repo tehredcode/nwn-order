659add32b02e13cff4a6c10814225d8849436722
urothis <natecaple@gmail.com>
2019-01-06 02:19:32 -0600

    New cron and new logs, still expanding.

--- main.go
+++ main.go
@@ -1,20 +1,17 @@
 package main
 
 import (
-	"crypto/rand"
 	"fmt"
 	"io/ioutil"
 	"net"
 	"net/http"
-	"os"
 	"strconv"
 	"time"
 
-	"github.com/robfig/cron"
 	"github.com/caarlos0/env"
-	"github.com/glendc/go-external-ip"
 	"github.com/go-redis/redis"
 	"github.com/google/go-github/github"
+	"github.com/robfig/cron"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -36,7 +33,71 @@ var (
 	RedisClient *redis.Client
 )
 
-func startPubsub() {
+func initMain() {
+	cfg := config{}
+	err := env.Parse(&cfg)
+
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+	}
+
+	log.WithFields(log.Fields{"Booted": 1}).Info("Order")
+
+	S := 0
+	conn, err := net.Dial("udp", "redis:6379")
+	for retry := 1; err != nil; retry++ {
+		S := strconv.Itoa(retry)
+		log.WithFields(log.Fields{"Connected": 0, "Attempt": S}).Warn("Order:Redis")
+		if retry > 4 {
+			log.WithFields(log.Fields{"Connected": 0, "Attempt": S}).Fatal("Order:Redis")
+		}
+		time.Sleep(5 * time.Second)
+	}
+	log.WithFields(log.Fields{"Connected": 1, "Attempt": S}).Info("Order:Redis")
+	conn.Close()
+
+	// start pubsub
+	go initPubsub()
+
+	go webserver()
+
+	c := cron.New()
+	if cfg.HbOneMinute == true {
+		c.AddFunc("@every 1m", func() { sendPubsub("Heartbeat", "heartbeat", "1") })
+	}
+	log.WithFields(log.Fields{"Heartbeat": "1", "Enabled": cfg.HbOneMinute}).Info("Heartbeat")
+	if cfg.HbFiveMinute == true {
+		c.AddFunc("@every 5m", func() { sendPubsub("Heartbeat", "heartbeat", "5") })
+	}
+	log.WithFields(log.Fields{"Heartbeat": "5", "Enabled": cfg.HbFiveMinute}).Info("Heartbeat")
+	if cfg.HbThirtyMinute == true {
+		c.AddFunc("@every 30m", func() { sendPubsub("Heartbeat", "heartbeat", "30") })
+	}
+	log.WithFields(log.Fields{"Heartbeat": "30", "Enabled": cfg.HbThirtyMinute}).Info("Heartbeat")
+	if cfg.HbOneHour == true {
+		c.AddFunc("@every 1h", func() { sendPubsub("Heartbeat", "heartbeat", "60") })
+	}
+	log.WithFields(log.Fields{"Heartbeat": "60", "Enabled": cfg.HbOneHour}).Info("Heartbeat")
+	if cfg.HbSixHour == true {
+		c.AddFunc("@every 6h", func() { sendPubsub("Heartbeat", "heartbeat", "360") })
+	}
+	log.WithFields(log.Fields{"Heartbeat": "360", "Enabled": cfg.HbSixHour}).Info("Heartbeat")
+	if cfg.HbTwelveHour == true {
+		c.AddFunc("@every 12h", func() { sendPubsub("Heartbeat", "heartbeat", "720") })
+	}
+	log.WithFields(log.Fields{"Heartbeat": "720", "Enabled": cfg.HbTwelveHour}).Info("Heartbeat")
+	if cfg.HbTwentyfourHour == true {
+		c.AddFunc("@every 24h", func() { sendPubsub("Heartbeat", "heartbeat", "1440") })
+	}
+	log.WithFields(log.Fields{"Heartbeat": "1440", "Enabled": cfg.HbTwentyfourHour}).Info("Heartbeat")
+	c.Start()
+
+	for {
+		time.Sleep(time.Second)
+	}
+}
+
+func initPubsub() {
 	cfg := config{}
 	err := env.Parse(&cfg)
 
@@ -55,46 +116,16 @@ func startPubsub() {
 		"debug",
 		"github",
 	)
+	log.WithFields(log.Fields{"heartbeat": "1", "input": "1", "debug": "1", "github": "1"}).Info("Order:Redis:Pubsub:Subscribe")
 	for {
 		msg, _ := pubSub.ReceiveMessage()
 		switch msg.Channel {
 		case "heartbeat":
 
 		case "input":
-			go uuidGeneration()
+
 		case "debug":
 		}
-	}
-}
-
-func uuidGeneration() {
-	cfg := config{}
-	err := env.Parse(&cfg)
-
-	if err != nil {
-		fmt.Printf("%+v\n", err)
-	}
-
-	client := redis.NewClient(&redis.Options{
-		Addr: "redis:" + cfg.RedisPort,
-	})
-	defer client.Close()
-
-	b := make([]byte, 16)
-	_, err = rand.Read(b)
-	if err != nil {
-		log.Fatal(err)
-	}
-	uuid := fmt.Sprintf("%x%x%x%x%x",
-		b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
-
-	if err := client.Set("system:uuid", uuid, 0).Err(); err != nil {
-		panic(err)
-	}
-
-	pub := client.Publish("output", "uuid")
-	if err = pub.Err(); err != nil {
-		fmt.Print("PublishString() error", err)
 	}
 }
 
@@ -115,16 +146,10 @@ func sendPubsub(LogMessage string, PubsubChannel string, PubsubMessage string) {
 		panic(err)
 	}
 
-	fmt.Println(LogMessage)
+	log.WithFields(log.Fields{"Channel": PubsubChannel, "Message": PubsubMessage}).Info(LogMessage)
 }
 
-func heartbeatWebhook(ticker string, verbose bool) {
-	t := time.Now()
-	msg := ("O [" + t.Format("15:04:05") + "] [NWN_Order] Pubsub Event: channel=heartbeat message=" + ticker)
-	sendPubsub(msg, "heartbeat", ticker)
-}
-
-func githubWebhook(w http.ResponseWriter, r *http.Request) {
+func recieveWebhook(w http.ResponseWriter, r *http.Request) {
 	payload, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		fmt.Printf("error validating request body: err=%s\n", err)
@@ -151,100 +176,21 @@ func githubWebhook(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func webpage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world!")
-}
-
 func webserver() {
 	cfg := config{}
 	err := env.Parse(&cfg)
-
 	if err != nil {
 		fmt.Printf("%+v\n", err)
 	}
 
-	consensus := externalip.DefaultConsensus(nil, nil)
-	ip, _ := consensus.ExternalIP()
-	http.HandleFunc("/webhook", githubWebhook)
-	t := time.Now()
-	fmt.Println("O [" + t.Format("15:04:05") + "] [NWN_Order] Boot Event: webserver started with external IP of " + ip.String() + ":" + cfg.OrderPort + ". webhooks need to be sent to /webhook")
+	http.HandleFunc("/webhook", recieveWebhook)
+	log.WithFields(log.Fields{"Started": 1, "Port": cfg.OrderPort}).Info("Order:Webserver")
 
-	http.HandleFunc("/", webpage)
-	log.Fatal(http.ListenAndServe(":"+cfg.OrderPort, nil))
+	http.ListenAndServe(":"+cfg.OrderPort, nil)
 }
 
 func main() {
-    done := make(chan bool)
+	done := make(chan bool)
 	go initMain()
-    <-done // Block forever
-}
-
-func initMain() {
-	cfg := config{}
-	err := env.Parse(&cfg)
-	if err != nil {
-		fmt.Printf("%+v\n", err)
-	}
-
-	t := time.Now()
-	log.Println("O [" + t.Format("15:04:05") + "] [NWN_Order] Boot Event: Order has Started")
-
-	conn, err := net.Dial("udp", "redis:6379")
-	for retry := 1; err != nil; retry++ {
-		trds := time.Now()
-		s := strconv.Itoa(retry)
-		fmt.Println("O [" + trds.Format("15:04:05") + "] [NWN_Order] Boot Event: Redis not connected | Retry attempt: " + s + " | 5 second sleep")
-		if retry > 4 {
-			fmt.Println("O [" + trds.Format("15:04:05") + "] [NWN_Order] Boot Event: Redis not connected | Exiting")
-			os.Exit(1)
-		}
-		time.Sleep(5 * time.Second)
-	}
-	conn.Close()
-	t = time.Now()
-	fmt.Println("O [" + t.Format("15:04:05") + "] [NWN_Order] Boot Event: Redis connected")
-
-	// start pubsub
-	go startPubsub()
-	fmt.Println("O [" + t.Format("15:04:05") + "] [NWN_Order] Boot Event: Pubsub started")
-
-	// start webhook reciever
-	go webserver()
-	fmt.Println("O [" + t.Format("15:04:05") + "] [NWN_Order] Boot Event: Webserver started")
-
-	c := cron.New()
-    if cfg.HbOneMinute == true {
-        c.AddFunc("@every 1m", func() { sendPubsub("Heartbeat", "heartbeat", "1") })
-    }
-    log.WithFields(log.Fields{"Heartbeat": "1", "Enabled": cfg.HbOneMinute}).Info("Heartbeat")
-    if cfg.HbFiveMinute == true {
-        c.AddFunc("@every 5m", func() { sendPubsub("Heartbeat", "heartbeat", "5") })
-    }
-    log.WithFields(log.Fields{"Heartbeat": "5", "Enabled": cfg.HbFiveMinute}).Info("Heartbeat")
-    if cfg.HbThirtyMinute == true {
-        c.AddFunc("@every 30m", func() { sendPubsub("Heartbeat", "heartbeat", "30") })
-    }
-    log.WithFields(log.Fields{"Heartbeat": "30", "Enabled": cfg.HbThirtyMinute}).Info("Heartbeat")
-    if cfg.HbOneHour == true {
-        c.AddFunc("@every 1h", func() { sendPubsub("Heartbeat", "heartbeat", "60") })
-    }
-    log.WithFields(log.Fields{"Heartbeat": "60", "Enabled": cfg.HbOneHour}).Info("Heartbeat")
-    if cfg.HbSixHour == true {
-        c.AddFunc("@every 6h", func() { sendPubsub("Heartbeat", "heartbeat", "360") })
-    }
-    log.WithFields(log.Fields{"Heartbeat": "360", "Enabled": cfg.HbSixHour}).Info("Heartbeat")
-    if cfg.HbTwelveHour == true {
-        c.AddFunc("@every 12h", func() { sendPubsub("Heartbeat", "heartbeat", "720") })
-    }
-    log.WithFields(log.Fields{"Heartbeat": "720", "Enabled": cfg.HbTwelveHour}).Info("Heartbeat")
-    if cfg.HbTwentyfourHour == true {
-        c.AddFunc("@every 24h", func() { sendPubsub("Heartbeat", "heartbeat", "1440") })
-    }
-    log.WithFields(log.Fields{"Heartbeat": "1440", "Enabled": cfg.HbTwentyfourHour}).Info("Heartbeat")
-	c.Start()
-
-	for {
-        fmt.Printf("%v+\n", time.Now())
-        time.Sleep(time.Second)
-    }
+	<-done // Block forever
 }
