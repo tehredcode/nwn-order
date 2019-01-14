f613535644f28212535c7d46bd2bcd070a03668d
Urothis <natecaple@gmail.com>
2019-01-14 14:12:02 -0600

    duplicate logs

--- plugin_log.go
+++ plugin_log.go
@@ -19,19 +19,7 @@ func initLog() {
 	// app started
 	log.WithFields(log.Fields{"Started": 1}).Info("Order:Log")
 
-	go watchNwserverLog()
 	go watchNwnxeeLog()
-}
-
-func watchNwserverLog() {
-	t, err := tail.TailFile("/logs/nwserverLog1.txt", tail.Config{Follow: true})
-	for line := range t.Lines {
-		log.WithFields(log.Fields{"": 1}).Info("Order:Log:Nwserver:" + line.Text)
-	}
-
-	if err != nil {
-		fmt.Printf("%+v\n", err)
-	}
 }
 
 func watchNwnxeeLog() {
