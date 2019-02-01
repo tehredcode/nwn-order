af34696a25d1c77f298e0028fcba6713c55c832f
Urothis <natecaple@gmail.com>
2019-02-01 00:57:52 -0600

    last one.

--- core_http.go
+++ core_http.go
@@ -28,6 +28,7 @@ func getServerStats(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(data)
+	return
 }
 
 func setServerStats(w http.ResponseWriter, r *http.Request) {
