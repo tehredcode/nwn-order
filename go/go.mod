d99007a7a29ca04c3978b99a1f64f246bef5f817
urothis <natecaple@gmail.com>
2019-02-04 23:12:15 -0600

    .

--- go/go.mod
+++ go/go.mod
@@ -2,7 +2,13 @@ module github.com/urothis/nwn-order
 
 require (
 	github.com/bwmarrin/discordgo v0.19.0
+	github.com/codegangsta/inject v0.0.0-20150114235600-33e0aa1cb7c0 // indirect
+	github.com/codegangsta/martini v0.0.0-20170121215854-22fa46961aab
+	github.com/garyburd/redigo v1.6.0
+	github.com/go-martini/martini v0.0.0-20170121215854-22fa46961aab // indirect
 	github.com/gorilla/mux v1.7.0
+	github.com/martini-contrib/render v0.0.0-20150707142108-ec18f8345a11
+	github.com/oxtoacart/bpool v0.0.0-20150712133111-4e1c5567d7c2 // indirect
 	github.com/sirupsen/logrus v1.3.0
 	github.com/urothis/nwn-order/go/api v0.0.0-20190204195419-918c3d59bb3a
 	github.com/urothis/nwn-order/go/log v0.0.0-20190204185925-bed01e80542e
--- go/main.go
+++ go/main.go
@@ -1,135 +1,78 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
-	"github.com/bwmarrin/discordgo"
-	"github.com/gorilla/mux"
-	logrus "github.com/sirupsen/logrus"
-	api "github.com/urothis/nwn-order/go/api"
-	log "github.com/urothis/nwn-order/go/log"
-	rds "github.com/urothis/nwn-order/go/redis"
+	"github.com/codegangsta/martini"
+	"github.com/garyburd/redigo/redis"
+	"github.com/martini-contrib/render"
+	log "github.com/sirupsen/logrus"
 )
 
-func initHTTP(c *rds.Client) {
-	r := mux.NewRouter()
-	r.HandleFunc("/webhook/dockerhub", api.DockerhubWebhookHandler)
-	r.HandleFunc("/webhook/github", api.GithubWebhookHandler)
-	r.HandleFunc("/webhook/gitlab", api.GitlabWebhookHandler)
-	r.HandleFunc("/api/server", redisHandler(c, getServerStats(c))).Methods("POST")
+var (
+	redisAddress   = flag.String("redis-address", ":6379", "Address to the Redis server")
+	maxConnections = flag.Int("max-connections", 10, "Max connections to Redis")
+)
 
-	http.ListenAndServe(":"+os.Getenv("NWN_ORDER_PORT"), r)
-	log.WithFields(log.Fields{"Port": os.Getenv("NWN_ORDER_PORT"), "Started": 1}).Info("Order:API")
-}
+func InitAPI() {
+	martini.Env = martini.Prod
 
-func initPubsub(c *rds.Client) {
-	pubSub, err := c.Subscribe()
-	err = pubSub.Subscribe(
-		"Discord:Out",
-		"Log:Debug",
-		"Log:Info",
-		"Log:Warning",
-		"Log:Fatal",
-	)
-	if err != nil {
-		log.WithFields(log.Fields{"Connected": "0", "Please confirm redis is connected": "1"}).Fatal("Order:Redis")
-	}
-	defer pubSub.Close()
-	log.WithFields(log.Fields{"Discord:Out": "1", "Log:Debug": "1", "Log:Info": "1", "Log:Warning": "1", "Log:Fatal": "1"}).Info("Order:Redis:Pubsub:Subscribe")
+	flag.Parse()
 
-	for {
-		msg, err := pubSub.ReceiveMessage()
+	redisPool := redis.NewPool(func() (redis.Conn, error) {
+		c, err := redis.Dial("tcp", *redisAddress)
+
 		if err != nil {
-			log.WithFields(log.Fields{"Connected": "0"}).Warn("Order:Redis:Pubsub")
+			return nil, err
 		}
-		switch msg.Channel {
-		case "Discord:Out":
-			log.WithFields(log.Fields{"Pubsub": "1", "Channel": msg.Channel, "Message": msg.Payload}).Info("Order:Pubsub")
-		case "Log:Debug":
-			log.WithFields(log.Fields{"Pubsub": "1", "Channel": msg.Channel, "Message": msg.Payload}).Info("Order:Pubsub")
-		case "Log:Info":
-			log.WithFields(log.Fields{"Pubsub": "1", "Channel": msg.Channel, "Message": msg.Payload}).Info("Order:Pubsub")
-		case "Log:Warning":
-			log.WithFields(log.Fields{"Pubsub": "1", "Channel": msg.Channel, "Message": msg.Payload}).Info("Order:Pubsub")
-		case "Log:Fatal":
-			log.WithFields(log.Fields{"Pubsub": "1", "Channel": msg.Channel, "Message": msg.Payload}).Info("Order:Pubsub")
-		}
-		fmt.Println(msg.Channel, msg.Payload)
-	}
-}
 
-func initDiscord(c *rds.Client) {
-	log.WithFields(log.Fields{"BotKey": os.Getenv("NWN_ORDER_PLUGIN_DISCORD_BOT_KEY"), "started": "1"}).Info("Order:Discord")
-	discord, err := discordgo.New("Bot " + os.Getenv("NWN_ORDER_PLUGIN_DISCORD_BOT_KEY"))
-	errCheck("error creating discord session", err)
-	user, err := discord.User("@me")
-	errCheck("error retrieving account", err)
+		return c, err
+	}, *maxConnections)
 
-	botID = user.ID
-	discord.AddHandler(inHandler)
-	discord.AddHandler(func(discord *discordgo.Session, ready *discordgo.Ready) {
-		err = discord.UpdateStatus(0, "Order")
+	defer redisPool.Close()
+
+	m := martini.Classic()
+
+	m.Map(redisPool)
+
+	m.Use(render.Renderer())
+
+	m.Get("/stats", func(r render.Render, pool *redis.Pool, params martini.Params) {
+		c := pool.Get()
+		defer c.Close()
+
+		k := "Order:server"
+		value1 := "Order"
+		value2, err := redis.String(c.Do("HGET", k, "BootTime"))
+		value3, err := redis.String(c.Do("HGET", k, "BootDate"))
+
 		if err != nil {
-			log.WithFields(log.Fields{"Set Status": "0"}).Info("Order:Discord:Error")
+			message := fmt.Sprintf("Could not GET %s", "key")
+
+			r.JSON(400, map[string]interface{}{
+				"status":  "ERR",
+				"message": message})
+		} else {
+			r.JSON(200, map[string]interface{}{
+				"status":     "OK",
+				"ModuleName": value1,
+				"BootTime":   value2,
+				"BootDate":   value3,
+			})
 		}
-		servers := discord.State.Guilds
-		log.WithFields(log.Fields{"Started": 1, "Clients connected": len(servers)}).Info("Order:Discord")
 	})
 
-	err = discord.Open()
-	errCheck("Error opening connection to Discord", err)
-	defer discord.Close()
-
-	commandPrefix = "!"
-
-	<-make(chan struct{})
+	m.Run()
 }
 
 func initMain() {
 	// app started
-	logrus.WithFields(logrus.Fields{"Booted": 1}).Info("Order")
-
-	// grab redis client
-	client := InitRedisClient()
-	rds := &RedisInstance{rds.Client: &client}
-
-	// start the web stuff
-	go initHTTP(rds)
-	logrus.WithFields(logrus.Fields{"API": 1}).Info("Order")
-
-	// start pubsub
-	go initPubsub(rds)
-	logrus.WithFields(logrus.Fields{"Pubsub": 1}).Info("Order")
-
-	// start plugins
-	go initPlugins(rds)
-}
-
-func initPlugins(rds *RedisInstance) {
-	if os.Getenv("NWN_ORDER_PLUGIN_DISCORD_ENABLED") == "1" {
-		logrus.WithFields(logrus.Fields{"Enabled": 1}).Info("Order:Discord")
-		go initDiscord(rds)
-	} else {
-		logrus.WithFields(logrus.Fields{"Enabled": 0}).Info("Order:Discord")
-	}
-
-	if os.Getenv("NWN_ORDER_PLUGIN_HEARTBEAT_ENABLED") == "1" {
-		logrus.WithFields(logrus.Fields{"Enabled": 1}).Info("Order:Heartbeat")
-		go initHeartbeat(rds)
-	} else {
-		logrus.WithFields(logrus.Fields{"Enabled": 0}).Info("Order:Heartbeat")
-	}
-
-	if os.Getenv("NWN_ORDER_PLUGIN_LOG_ENABLED") == "1" {
-		logrus.WithFields(logrus.Fields{"Enabled": 1}).Info("Order:log")
-		go initLog(rds)
-	} else {
-		logrus.WithFields(logrus.Fields{"Enabled": 0}).Info("Order:log")
-	}
+	log.WithFields(log.Fields{"Booted": 1}).Info("Order")
+	go InitAPI()
 }
 
 func main() {
